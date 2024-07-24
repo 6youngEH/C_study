43944fc3575b440d5a192e795e977f9cd5a1497e
6youngEH <a42230@naver.com>
2024-07-24 14:51:56 +0900

    first commit

---
+++ c연습/Backjune1085.c
@@ -0,0 +1,26 @@
+//직사각형에서 탈출 (2024/07/24)
+#include <stdio.h>
+
+int main(void)
+{
+     int x,y,w,h;
+
+     scanf("%d %d %d %d",&x,&y,&w,&h);
+     int min1,min2;
+     if(x<w-x)
+        min1 = x;
+     else
+        min1 = w-x;
+
+     if(y<h-y)
+        min2 = y;
+     else
+        min2 = h-y;
+
+     if(min1 < min2)
+        printf("%d\n",min1);
+     else
+        printf("%d\n", min2);
+    
+}
+
